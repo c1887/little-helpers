56c378dfb2fb56c36c6503880623c934e72257dd
Lasse Hinrichsen <lh1887@mi.fu-berlin.de>
2017-10-12 16:19:53 +0200

    Add typedef to using converter script

---
+++ killAllTypedefs/killalltypedefs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# simple script to replace C++ lines like
+# '  typedef SOMETHING BLABLA foo;'
+# by the newer "using" keyword:
+# '  using foo = SOMETHING BLABLA;'
+
+# Usage: ./killalltypedefs FILENAME1 [FILENAME2 ...]
+
+import fileinput
+
+with fileinput.input(inplace=True) as f:
+    for line in f:
+        if (len(line.split())==0):
+            print(line, end='')
+            continue
+
+        if (line.split()[0] == 'typedef'):
+            # save intendation
+            leading = line[:-len(line.lstrip())-1]
+            # save the alias' name
+            name = line.rstrip().rstrip(";").split()[-1]
+            wordlist = [leading, "using", name, "="]
+            # extend by what is going to be aliased
+            wordlist.extend(line.strip().split()[1:-1])
+            string = " ".join(wordlist)
+            print(string+";")
+        else:
+            print(line, end='')
+
+fileinput.close()
+
